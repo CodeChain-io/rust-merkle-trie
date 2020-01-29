54e81198a452d112b3f20c6f4146adfce384b8cb
Junha Yang0 <junha@codechain.io>
2020-01-29 19:28:15 +0900

    proof of presence

--- src/proof.rs
+++ src/proof.rs
@@ -15,52 +15,122 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use crate::nibbleslice::NibbleSlice;
-use crate::node::Node;
+use crate::node::Node as Node;
 use crate::{Trie, TrieError};
 use ccrypto::{blake256, BLAKE_NULL_RLP};
 use cdb::HashDB;
 use primitives::H256;
 
-macro_rules! CRYPTOPROOF_ALL_EQ_CLONE {
-    () => {
-    H : PartialEq + Clone,
-    P : PartialEq + Clone,
-    V : PartialEq + Clone};
-}
-
-
 // Unit of proof.
 //#[derive(Clone, Eq, PartialEq, Debug, RlpEncodable, RlpDecodable)]
 #[derive(Clone, Eq, PartialEq, Debug)]
-pub struct CryptoProofUnit<H, P, V>
+pub struct CryptoProofUnit<H, K, V>
 {
     hash: H, // or root
-    path: P,
+    key: K,
     value: V,
     presence: bool
 }
 
 // Abstract trait of being cryptographically provable.
-pub trait CryptoProvable<H, P, V>
+pub trait CryptoProvable<H, K, V>
 {
     // Does this verify the unit?
-    fn verify(&self, test: &CryptoProofUnit<H, P, V>) -> bool;
+    fn verify(&self, test: &CryptoProofUnit<H, K, V>) -> bool;
 }
 
 // This is strongly bound to implementation of TrieDB. I want to specify that but rust has no HKT(?)
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Clone, Eq, PartialEq, Debug)]
 pub struct CryptoProof_MerkleTrie {
-    proof: Vec<Vec<u8>>
+    proof: Vec<Vec<u8>> // from Root to Leaf
 }
 
-impl CryptoProvable<H256, H256, &[u8]> for CryptoProof_MerkleTrie {
-    // Proof should start with leaf node.
-    fn verify(&self, test: &CryptoProofUnit<H256, H256, &[u8]>) -> bool {
-        match self.proof.get(0) {
-            None => return false,
-            Some(elem) => {
-                return false; // TODO
+impl CryptoProof_MerkleTrie {
+    fn verify_p(&self, test: &CryptoProofUnit<H256, H256, Vec<u8>>) -> bool {
+        type Tunit = CryptoProofUnit<H256, H256, Vec<u8>>;
+
+        /*
+        Here the assumption is that 
+        - Don't trust RLP decoding
+        */
+
+        // step1: verify the value
+        fn step1(self_: &CryptoProof_MerkleTrie, test: &Tunit) -> bool {
+            match self_.proof.get(self_.proof.len() - 1) {
+                None => false, // empty proof
+                Some(elem) => {
+                    match Node::decoded(elem) {
+                        Some(x) => match x {
+                            Node::Leaf(_, value) => {
+                                if test.value == value {
+                                    true
+                                } else {
+                                    false
+                                }
+                            },
+                            _ => false
+                        },
+                        _ => false
+                    }
+                }
             }
+        };
+
+        // step2: verify the root
+        fn step2(self_: &CryptoProof_MerkleTrie, test: &Tunit) -> bool {
+            match self_.proof.get(0) {
+                None => false,
+                Some(elem) => blake256(elem) == test.hash
+            }
+        };
+
+        // step3: verify the key
+        fn step3(self_: &CryptoProof_MerkleTrie, test: &Tunit) -> bool {
+            let path = NibbleSlice::new(&blake256(test.key));
+            /*
+            Invariants
+            */
+            fn verify_branch(path: &NibbleSlice<'_>, proof: &[Vec<u8>]) -> bool {
+                match Node::decoded(&proof[0]) {
+                    Some(x) => match x {
+                        Node::Leaf(ns, _) => {
+                            *path == ns
+                        },
+                        Node::Branch(ns, table) => {
+                            let nibble = path.at(0);
+
+                            // Note: Does Rust guarantee the short circuit evaluation?
+                            proof.len() >= 2 && nibble == ns.at(0) && match table[nibble as usize] {
+                                Some(x) => x == blake256(&proof[1]),
+                                _ => false
+                            } &&  {
+                                let next_nibble = path.clone();
+                                next_nibble.offset += 1;
+                                verify_branch(&next_nibble, &proof[1..])
+                            }
+                        }
+                    },
+                    _ => false
+                }
+            };
+            verify_branch(&path, &self_.proof)
+        };
+        step1(self, test) && step2(self, test) && step3(self, test)
+    }
+
+    fn verify_a(&self, test: &CryptoProofUnit<H256, H256, Vec<u8>>) -> bool {
+        false
+    }
+}
+
+impl CryptoProvable<H256, H256, Vec<u8>> for CryptoProof_MerkleTrie {
+    // Proof should start with leaf node.
+    fn verify(&self, test: &CryptoProofUnit<H256, H256, Vec<u8>>) -> bool {
+        if test.presence {
+            self.verify_p(test)
+        }
+        else {
+            self.verify_a(test)
         }
     }
 }
@@ -71,38 +141,37 @@ it is self-proven and doesn't require any further verification.
 It is useful in non-serializing context.
 This is enclosed by the module to prevent arbitrary struct initialization.
 */
-mod Verified {
-    pub trait CryptoProvable<H, P, V, Pf> : super::CryptoProvable<H, P, V> {
-        fn construct_and_verify(unit: &super::CryptoProofUnit<H, P, V>, proof: &Pf) -> Option<Box<Self>>;
+mod verified {
+    pub trait CryptoProvable<H, K, V, P> : super::CryptoProvable<H, K, V> {
+        fn construct_and_verify(unit: &super::CryptoProofUnit<H, K, V>, proof: &P) -> Option<Box<Self>>;
     }
 
-    pub struct CryptoProof<H, P, V> {
-        unit: super::CryptoProofUnit<H, P, V>,
+    pub struct CryptoProof<H, K, V> {
+        unit: super::CryptoProofUnit<H, K, V>,
     }
 
-    impl<H, P, V> super::CryptoProvable<H, P, V> for CryptoProof<H, P, V> where
+    impl<H, K, V> super::CryptoProvable<H, K, V> for CryptoProof<H, K, V> where
         H : PartialEq + Clone,
-        P : PartialEq + Clone,
+        K : PartialEq + Clone,
         V : PartialEq + Clone
     {
-        fn verify(&self, test: &super::CryptoProofUnit<H, P, V>) -> bool {
+        fn verify(&self, test: &super::CryptoProofUnit<H, K, V>) -> bool {
             return self.unit == *test;
         }
     }
 
-    impl<H, P, V, Pf> CryptoProvable<H, P, V, Pf> for CryptoProof<H, P, V> where
+    impl<H, K, V, P> CryptoProvable<H, K, V, P> for CryptoProof<H, K, V> where
         H : PartialEq + Clone,
-        P : PartialEq + Clone,
+        K : PartialEq + Clone,
         V : PartialEq + Clone,
-        Pf : super::CryptoProvable<H, P, V>
+        P : super::CryptoProvable<H, K, V>
     {
-        fn construct_and_verify(unit: &super::CryptoProofUnit<H, P, V>, proof: &Pf) -> Option<Box<Self>> {
+        fn construct_and_verify(unit: &super::CryptoProofUnit<H, K, V>, proof: &P) -> Option<Box<Self>> {
             if proof.verify(unit) {
                 None
             }
             else {
-                let www = unit.clone();
-                Some(Box::new(Self{unit: www}))
+                Some(Box::new(Self{unit: unit.clone()}))
             }
         }
     }
